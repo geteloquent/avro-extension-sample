0bf3cfde0798f7dca0a08a3d13a482fff8c65350
Tiago Lima <fltiago@gmail.com>
2015-11-25 11:35:46 -0300

    Revert "Try to use custom tool inside Velocity template"
    This reverts commit 4d2ca7032553ff4bbbd83d265424e3b22b248c4f.

--- src/main/java/velocity-tools/ValidationsGenerator.java
+++
@@ -1,9 +0,0 @@
-package velocitytools;
-
-import static org.apache.avro.Schema.Field;
-
-public class ValidationsGenerator {
-  public String validationsFor(Field field) {
-    return field.name() + " should not be null.";
-  }
-}
